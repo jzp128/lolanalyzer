d6927541a7fd1559908f027b1b0227cbd4a1dd2b
Jimmy <jimmypang8@gmail.com>
2021-02-13 21:44:21 -0500

    Added champion checker

--- server/analyzer/matchchecker.ts
+++ server/analyzer/matchchecker.ts
@@ -1,36 +1,22 @@
+import {getChampionKeyWithName} from "../../utils/ChampionUtils";
+
 export default class MatchChecker {
     private readonly matches: Array<any>;
     private playerMatchesDetail: Array<any>;
     private playerName: string;
     private playerId: string;
 
+    // key pairing of matchid: participantId
+    private participantId: {};
+
     constructor(matches: Array<any>, playerName: string, playerId: string) {
         this.matches = matches;
         this.playerId = playerId;
         this.playerName = playerName;
         this.playerMatchesDetail = [];
+        this.participantId = {};
         this.getMatchesForPlayer();
     }
-
-    test() {
-        return this.matches[0].participantIdentities;
-    }
-
-    findUser(summonerName: string) {
-
-    }
-
-    calculateDamageInNGames(gameNum: number) {
-        let games: number = Math.min(gameNum, this.matches.length);
-        let totalDmg: number = 0;
-        for (let i = 0; i < games; i++) {
-            let dmg = this.playerMatchesDetail[i].stats.totalDamageDealtToChampions;
-            totalDmg += dmg;
-        }
-        console.log(totalDmg);
-        return totalDmg;
-    }
-
 
     private getMatchesForPlayer() {
         for (let i = 0; i < this.matches.length; i++) {
@@ -42,10 +28,52 @@ export default class MatchChecker {
                 let p = participants[j].player;
                 if (p.summonerName === this.playerName) {
                     this.playerMatchesDetail.push(match.participants[j]);
+                    this.participantId[match.gameId] = p.participantId;
                     break;
                 }
             }
 
         }
     }
+
+    getKaisaGames() {
+        const kaisaID = getChampionKeyWithName('Kaisa');
+        let kaisagames = [];
+        // gets matches where someone's played kai'sa
+        for (let i = 0; i < this.playerMatchesDetail.length; i++) {
+            let matchDetail = this.playerMatchesDetail[i];
+            if (matchDetail.championId === kaisaID) {
+                kaisagames.push(matchDetail);
+            }
+        }
+
+        // checks the builds that the kiasa games u went
+        for (let i = 0; i < kaisagames.length; i++) {
+            let matchDetail = this.playerMatchesDetail[i];
+            if (matchDetail.championId === kaisaID) {
+                kaisagames.push(this.playerMatchesDetail);
+            }
+        }
+
+    }
+
+    getGamesOnChampion() {
+
+    }
+
+    getExhuastGames() {
+
+    }
+
+    calculateDamageInNGames(gameNum: number) {
+        let games: number = Math.min(gameNum, this.matches.length);
+        let totalDmg: number = 0;
+        for (let i = 0; i < games; i++) {
+            let dmg = this.playerMatchesDetail[i].stats.totalDamageDealtToChampions;
+            totalDmg += dmg;
+        }
+        return totalDmg;
+    }
+
+
 }
